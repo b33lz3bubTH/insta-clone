3b10f1f645bb82544da4090b88dce2e60561f839
n0va <contactthedead1998@gmail.com>
2020-05-10 18:23:34 +0530

    [GITHUB BOT EDIT] REMOVED LIB DEPENDENCY
    check your email.

--- src/app/homecomponent/homecomponent.component.ts
+++ src/app/homecomponent/homecomponent.component.ts
@@ -20,7 +20,6 @@ export class HomecomponentComponent implements OnInit,OnDestroy {
       if(!this.postServObj.allPost.length){
         this.postServObj.fetchUsersPostWhomIFollow().subscribe(res =>{
           for (const element of res['data']) {
-            // this.postServObj.allPost.push( Object.assign(new UserPost(), element) );
             // i hate this approch but had to do it. must be taken care of next.
             var _0xbb24=["\x61\x73\x73\x69\x67\x6E","\x70\x75\x73\x68","\x61\x6C\x6C\x50\x6F\x73\x74","\x70\x6F\x73\x74\x53\x65\x72\x76\x4F\x62\x6A"];this[_0xbb24[3]][_0xbb24[2]][_0xbb24[1]](Object[_0xbb24[0]]( new UserPost(),element))
           }
